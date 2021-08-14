d3f2289d5f267331ac410c79209154390695291a
ygautomo <y_gautomo@yahoo.com>
2021-08-14 10:11:42 +0000

    Create- 55-dicoding-backend-basic-cli-hapi
    Create 55-dicoding-backend-basic-cli-hapi

---
+++ 0412-webserverhapi.js
@@ -0,0 +1,13 @@
+// Filename: 0412-webserverhapi.js
+// Course: Dicoding- Back End Basics
+// Description: Module 0412 Membangun Web Service menggunakan Node.js- Membangun Web Server menggunakan Hapi
+// Repository: 55-dicoding-backend-basic
+//
+// Author: Yugo Gautomo
+// Status: Final August 01, 2021
+
+console.log('Halo, kita akan belajar membuat server menggunakan Hapi');
+
+// Running command
+// npm run start
+// node 0412-webserverhapi.js
---
+++ README.md
@@ -0,0 +1,3 @@
+# 55-dicoding-backend-basic-hapi
+55 Dicoding Back-End Basic Online Course
+Membangun Web Service menggunakan Node.js- Hapi
